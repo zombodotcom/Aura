d886ee429a6f90545dc78f8bdc356c2f42d3c507
Drew André <dandre@conncoll.edu>
2017-02-27 21:30:17 -0500

    updated

--- src/splatter.ino
+++ src/splatter.ino
@@ -2,50 +2,40 @@
 
 void splatter() {
 
+  maxChanges = 20;      // Value for blending between palettes.
+
+
   changePalette();
+  INDEX = random(10,240);
 
   //MILLISECONDS  = 10;
   //lowPass_audio = 0.15;
-
   //READ_AUDIO();
 
-  EVERY_N_MILLISECONDS(1000) {
-    hue+=10;
+  EVERY_N_MILLISECONDS(40){
+    fadeToBlackBy(leds,NUM_LEDS,1);
+    blur1d(leds, NUM_LEDS, 115);
   }
 
-//EVERY_N_MILLISECONDS(15){
-  //fadeToBlackBy(leds,NUM_LEDS,1);
-//}
+    //MILLISECONDS = beatsin8(50,20,400); // oscillating sin wave controlling
+                                          // raindrop drawing every 20-400ms, 50bmp
+    EVERY_N_MILLISECONDS(250) {
 
-
-  //fadeToBlackBy( leds, NUM_LEDS, 1);
-
-  blur1d(leds, NUM_LEDS, 100);
-
-  //for (int band = 0; band < 7; band++) {
-
-    //hue = band * 36;
-    //INDEX = random(0, 8);
-
-    //hue = ColorFromPalette(gCurrentPalette, INDEX, 255, LINEARBLEND);
-
-    //MILLISECONDS = beatsin8(1,40,800);
-    EVERY_N_MILLISECONDS(50) {
-      //if (mono[band] > 10) {
+      //if (mono[band] */ or mono_volume /*  > 10) { // if audio crosses a threshold
       pos = random16(NUM_LEDS - 1);
-      left_pos  = pos - random(2, 10);
-      right_pos = pos + random(2, 10);
+      left_pos  = pos - random(4, 14);
+      right_pos = pos + random(4, 14);
       if (left_pos < 0) {
         left_pos = 0;
       }
       if (right_pos > NUM_LEDS - 1) {
         right_pos = NUM_LEDS - 1;
       }
-      //}
-    }
+      for (int i = left_pos; i < right_pos; i++) {
+        leds[i] += ColorFromPalette(gCurrentPalette, INDEX, 150, LINEARBLEND);
 
-    for (int i = left_pos; i < right_pos; i++) {
-      leds[i] = CHSV( hue, 220, 220);
+      }
+      //}
     }
 
 
