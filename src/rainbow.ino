c7efc1399fc028889428f68a676c50209d741097
Drew André <dandre@conncoll.edu>
2017-03-01 20:18:28 -0500

    tenth

--- src/rainbow.ino
+++ src/rainbow.ino
@@ -4,29 +4,30 @@ int state = 0;
 
 void rainbow() {
 
-  hue = 0;
-  EVERY_N_MILLISECONDS(50) {
-    ihue--;
-    //if (ihue > 255) ihue = 0;
-  }
-  fill_rainbow(leds, NUM_LEDS, hue, 1);
+        hue = 0;
+        EVERY_N_MILLISECONDS(50) {
+                ihue--;
+                //if (ihue > 255) ihue = 0;
+        }
 
-  for(int i = 0; i < NUM_LEDS; i++){
-    hue = hue + 3;
-    if (hue > 255) hue = 0;
-    leds[i] = CHSV(hue + ihue, saturation,255);
-  }
+        fill_rainbow(leds, NUM_LEDS, hue, 1);
 
- if(state == 0){
-   saturation -= 7;
-   if(saturation < 10) state = 1;
- }
+        for(int i = 0; i < NUM_LEDS; i++) {
+                hue = hue + 3;
+                if (hue > 255) hue = 0;
+                leds[i] = CHSV(hue + ihue, saturation, 255);
+        }
 
- if(state == 1){
-    saturation += 7;
-    if(saturation > 245) state = 0;
- }
+        // if(state == 0){
+        //   saturation -= 7;
+        //   if(saturation < 10) state = 1;
+        // }
+        //
+        // if(state == 1){
+        //    saturation += 7;
+        //    if(saturation > 245) state = 0;
+        // }
 
-  //addGlitter(80);
+        //addGlitter(80);
 
 }
--- src/splatter.ino
+++ src/splatter.ino
@@ -2,41 +2,41 @@
 
 void splatter() {
 
-  maxChanges = 20;  // Value for blending speed between palettes
+        maxChanges = 20;         // Value for blending speed between palettes
 
-  changePalette();
-  INDEX = random(10,240);
+        changePalette();
+        INDEX = random(10,240);
 
-	// random16_add_entropy (uint16_t entropy)
+        // random16_add_entropy (uint16_t entropy)
 
-  EVERY_N_MILLISECONDS(52){
-    fadeToBlackBy(leds,NUM_LEDS,1);
-    blur1d(leds, NUM_LEDS, 140);
-  }
+        EVERY_N_MILLISECONDS(52){
+                fadeToBlackBy(leds,NUM_LEDS,1);
+                blur1d(leds, NUM_LEDS, 140);
+        }
 
-    //MILLISECONDS = beatsin8(50,20,400); // oscillating sin wave controlling
-                                          // raindrop drawing every 20-400ms, 50bmp
-    EVERY_N_MILLISECONDS(40) {
+        //MILLISECONDS = beatsin8(50,20,400); // oscillating sin wave controlling
+        // raindrop drawing every 20-400ms, 50bmp
+        EVERY_N_MILLISECONDS(40) {
 
-      //if (mono[band] */ or mono_volume /*  > 10) { // if audio crosses a threshold
-      pos = random16(NUM_LEDS - 1);
-      left_pos  = pos - random(15); // possibly change back to (min, max)
-      right_pos = pos + random(15);
-      if (left_pos < 0) {
-        left_pos = 0;
-      }
-      if (right_pos > NUM_LEDS - 1) {
-        right_pos = NUM_LEDS - 1;
-      }
-      for (int i = left_pos; i < right_pos; i++) {
-        leds[i] += ColorFromPalette(gCurrentPalette, INDEX, 150, LINEARBLEND);
+                //if (mono[band] */ or mono_volume /*  > 10) { // if audio crosses a threshold
+                pos = random16(NUM_LEDS - 1);
+                left_pos  = pos - random(15);                // possibly change back to (min, max)
+                right_pos = pos + random(15);
+                if (left_pos < 0) {
+                        left_pos = 0;
+                }
+                if (right_pos > NUM_LEDS - 1) {
+                        right_pos = NUM_LEDS - 1;
+                }
+                for (int i = left_pos; i < right_pos; i++) {
+                        leds[i] += ColorFromPalette(gCurrentPalette, INDEX, 150, LINEARBLEND);
 
-      }
-      //} // end audio threshold
-    }
+                }
+                //} // end audio threshold
+        }
 
 
-  }
+}
 
 // void addGlitter( fract8 chanceOfGlitter)
 // {
